af2443a30b2643a0abf2e154b8ae499f602ee647
Maryam Nadeali <m.nadeali95@gmail.com>
2022-05-15 13:10:46 +0430

    complete supplier

--- Supplier/admin.py
+++ Supplier/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Residence,Service
+from .models import Residence, Service, Tag, RestaurantMenu, ResidenceIndoorAlbum, ResidenceOutdoorAlbum
 
 
 
 admin.site.register(Residence)
 admin.site.register(Service)
+admin.site.register(Tag)
+admin.site.register(RestaurantMenu)
+admin.site.register(ResidenceIndoorAlbum)
+admin.site.register(ResidenceOutdoorAlbum)
